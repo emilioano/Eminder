e25df636dc5bb5f23a793164c301dd63b406ce8f
emilioano <emil.sjoekvist@gmail.com>
2026-01-11 17:34:51 +0100

    Tests

--- tests/test_dbactions.py
+++ tests/test_dbactions.py
@@ -7,7 +7,8 @@ from eminder.config import DBCONFIG
 
 from unittest.mock import patch
 
-# Testing that the Read operations from db does not result in error.
+# Testing that the Read operations from db does not result in error. This test runs only locally
+@pytest.mark.skip_in_ci
 @patch("eminder.db.dbactions.error")
 def test_read_db(mock_error):
     viewtasks()
